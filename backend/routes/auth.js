2e81ed535684bb7b452fc8bb15c384cb1493ac95
Sachi Chalmers <sachichalmers@Mac.lan>
2025-03-24 11:11:33 -0400

    Redo
    Starting Project over

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { login, getSpotifyData } = require("../controllers/authController");
-const router = express.Router();
-
-router.get("/login", login);
-router.get("/spotify-data", getSpotifyData);
-
-module.exports = router;
