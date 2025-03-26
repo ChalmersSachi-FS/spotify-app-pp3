81b009d3cba89840218ecf81082e9e598ff736f3
Sachi Chalmers <sachichalmers@Mac.lan>
2025-03-26 17:06:59 -0400

    REDO

--- backend/generate-jwt-secret.js
+++
@@ -1,5 +0,0 @@
-const crypto = require("crypto");
-
-// Generate a random 256-bit (32 bytes) secret in base64 encoding
-const jwtSecret = crypto.randomBytes(32).toString("base64");
-console.log(jwtSecret);
